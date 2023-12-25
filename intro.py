b15c4e874492ac6fecbaf5fc36c94e72f14e6af9
sieun <susanpark0@snu.ac.kr>
2023-12-25 22:46:58 +0900

    added files

---
+++ intro.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+st.title("AIED Research")
+
+"https://github.com/blackary/st_pages "
+# 시간 되면 설정하기 
+
+"1. 개요"
+
+"페이지의 안내를 따라가면 수업안의 전체 구성을 경험해 볼 수 있습니다."
+
+"2. 교사용 가이드라인 링크"
+
+"3. GitHub 링크 "
+
+"github clone ...."
+
+
+
+"사용 방법"
+"conda env.... "
